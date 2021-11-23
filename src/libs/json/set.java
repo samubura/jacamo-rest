1027f1d4980a0041351e352477653084c8134e89
Samuele <samuele.bura98@gmail.com>
2021-11-23 16:09:20 +0100

    Change set operation to require type for casting the right value

--- src/libs/json/set.java
+++ src/libs/json/set.java
@@ -8,8 +8,9 @@ import jason.asSyntax.*;
 import utils.AbstractInternalAction;
 
 /**
- * Use as set_field(json_obj, key, value)
+ * Use as set_field(json_obj, type, key, value)
  * - json_obj must be an object reference of a JsonObject or a JsonArray
+ * - type must be either ["number", "integer", "string", "object", "array", "boolean"] or _ and get the default behaviour
  * - key must be a string or a number or a _ to append to an array ignoring the index
  * - value can be either:
  *  + an atom with the value true/false for boolean
@@ -23,70 +24,77 @@ import utils.AbstractInternalAction;
 public class set extends AbstractInternalAction {
     @Override
     public int getMaxArgs() {
-        return 3;
+        return 4;
     }
 
     @Override
     public int getMinArgs() {
-        return 3;
+        return 4;
     }
 
     @Override
     protected Object action(TransitionSystem ts, Unifier un, Term[] args) throws JasonException {
         checkArguments(args);
-        if(args[1].isString()){
-            this.setObjectField((ObjectTerm)args[0], (StringTerm)args[1], args[2]);
-        } else if(args[1].isNumeric()){
-            this.setArrayElement((ObjectTerm)args[0], (NumberTerm)args[1], args[2]);
-        } else if(args[1].isUnnamedVar()) {
-            this.appendArrayElement((ObjectTerm)args[0], args[2]);
+        String type = ((StringTerm)args[1]).getString();
+        if(args[2].isString()){
+            this.setObjectField((ObjectTerm)args[0], (StringTerm)args[2], type, args[3]);
+        } else if(args[2].isNumeric()){
+            this.setArrayElement((ObjectTerm)args[0], (NumberTerm)args[2], type,  args[3]);
+        } else if(args[2].isUnnamedVar()) {
+            this.appendArrayElement((ObjectTerm)args[0],type, args[3]);
         }
         return true;
     }
 
-    private void setObjectField(ObjectTerm objTerm, StringTerm keyTerm, Term valueTerm) throws JasonException {
+    private void setObjectField(ObjectTerm objTerm, StringTerm keyTerm, String type, Term valueTerm) throws JasonException {
         try {
             JsonObject json = ((JsonElement) objTerm.getObject()).getAsJsonObject();
             String key = keyTerm.getString();
-            json.add(key, generateJsonElement(valueTerm));
+            json.add(key, generateJsonElement(type, valueTerm));
         } catch (IllegalStateException e){
             throw new JasonException("First argument was not a JsonObject");
         }
     }
 
-    private void setArrayElement(ObjectTerm objTerm, NumberTerm indexTerm, Term valueTerm) throws JasonException {
+    private void setArrayElement(ObjectTerm objTerm, NumberTerm indexTerm, String type, Term valueTerm) throws JasonException {
         try{
             JsonArray json = ((JsonElement)objTerm.getObject()).getAsJsonArray();
             int index = (int)indexTerm.solve();
-            json.set(index, generateJsonElement(valueTerm));
+            json.set(index, generateJsonElement(type, valueTerm));
         } catch (IllegalStateException e){
             throw new JasonException("First argument was not a JsonArray");
         }
     }
 
-    private void appendArrayElement(ObjectTerm objTerm, Term valueTerm) throws JasonException {
+    private void appendArrayElement(ObjectTerm objTerm, String type, Term valueTerm) throws JasonException {
         try{
             JsonArray json = ((JsonElement)objTerm.getObject()).getAsJsonArray();
-            json.add(generateJsonElement(valueTerm));
+            json.add(generateJsonElement(type, valueTerm));
         } catch (IllegalStateException e){
             throw new JasonException("First argument was not a JsonArray");
         }
     }
 
-    private JsonElement generateJsonElement(Term valueTerm) throws JasonException {
+    private JsonElement generateJsonElement(String type, Term valueTerm) throws JasonException {
         JsonElement value = null;
         if(valueTerm.isAtom()){
             //either a boolean or an obj reference
             Atom term = (Atom)valueTerm;
             String functor = term.getFunctor();
-            if(functor.equals("true") || functor.equals("false")) {
+            if(type.equals("boolean")){
                 //is a boolean
                 value = new JsonPrimitive(Boolean.parseBoolean(functor));
             }
         }
         else if(valueTerm.isNumeric()){
             NumberTerm term = (NumberTerm)valueTerm;
-            value = new JsonPrimitive(term.solve());
+            double argument = term.solve();
+            if(type.equals("integer")) {
+                value = new JsonPrimitive((int)argument);
+            } else {
+                value = new JsonPrimitive(argument);
+            }
+
         }
         else if(valueTerm.isString()) {
             StringTerm term = (StringTerm) valueTerm;
